7315f87a4a7de12de650d401d95af208cc11cdf6
Nedoborenko Kirill <k.nedoborenko@ddos-guard.net>
2026-04-30 12:10:21 +0300

    fix: router base path for GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,14 @@ import HomeView from '@/views/HomeView.vue';
 import CreateView from '@/views/CreateView.vue';
 import EditView from '@/views/EditView.vue';
 
-const routes = [
-  { path: '/', name: 'home', component: HomeView },
-  { path: '/create', name: 'create', component: CreateView },
-  { path: '/edit/:id', name: 'edit', component: EditView },
-];
+const router = createRouter({
+  // Для GitHub Pages нужен правильный базовый путь
+  history: createWebHistory('/ddg-test1/'),
+  routes: [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/create', name: 'create', component: CreateView },
+    { path: '/edit/:id', name: 'edit', component: EditView },
+  ],
+});
 
-const router = createRouter({ history: createWebHistory(), routes });
 export default router;
